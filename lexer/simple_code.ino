75019a0f449c7d800efe28e478c1c53ab826f786
Edgar Castro <edgarcm1908@gmail.com>
2020-09-30 00:22:03 -0500

    Separados lod grupos de general a mas especificos (i.e de solo numero a interos y de punto flotante)

--- lexer/simple_code.ino
+++ lexer/simple_code.ino
@@ -1,8 +1,12 @@
 int led = 13; // the pin the LED is connected to
 int algo = 0x3dfe45;
+float x = 0.13f;
 void setup() {
   pinMode(led, OUTPUT) // Declare the LED as an output
 }
+
+led++;
+led += 3;
 /*
 nada más para ver
 */
