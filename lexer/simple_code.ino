6320b89a8b9fa46284a0da0ca425d4182b3497fc
Edgar Castro <edgarcm1908@gmail.com>
2020-09-30 12:49:18 -0500

    Operador ternario agregado

--- lexer/simple_code.ino
+++ lexer/simple_code.ino
@@ -1,6 +1,6 @@
 int led = 13; // the pin the LED is connected to
 int algo = 0x3dfe45;
-float x = 0.13f;
+float x = 0.13f ? algo < 1 : 3.4f;
 void setup() {
   pinMode(led, OUTPUT) // Declare the LED as an output
 }
