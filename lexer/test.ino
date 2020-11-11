58d1bb2924f721d454f446c1593ba0d24a2c39e5
Edgar Castro <edgarcm1908@gmail.com>
2020-11-11 12:30:38 -0600

    Agregada la funcon que guarda la tabla en un archivo csv y se le dio formato a las producciones

--- lexer/test.ino
+++ lexer/test.ino
@@ -1,12 +1,14 @@
-#include int
-#include "mi_lib.h"
-/*
-long int _test = 3L;
-int binario = 0b01001;
-int hex = 0x43ef << binario;
+//#include int
+//#include "mi_lib.h"
+
+//long int _test = 3L;
+
+//int binario = 0b01001;
+//int hex = 0x43ef << binario;
 while(true){
-    pinMode(13, HIGH);
+    a += 0;
 }
+/*
 Serial.write("Hola mundo");
 binario += 0b0110;
 >=
