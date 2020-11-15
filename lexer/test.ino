10369852b132326b33b20d82582bf11ea4d6c79c
EskerOn <35613070+EskerOn@users.noreply.github.com>
2020-11-15 15:59:57 -0600

    Ajustes en la gramatica
    Estructuras if else funcionando

--- lexer/test.ino
+++ lexer/test.ino
@@ -1,32 +1,13 @@
-for(i=0; a<5; i++){
-  var++;
-}
-
-do{
-  var2 = 3;
-}while(c<5)
-
-/*
-int algo = 0x3dfe45;
-float x = 0.13f ? algo < 1 : 3.4f;
-void setup() {
-  pinMode(led, OUTPUT) 
-  Serial.begin(9600);
-}
-
-led++; //Comentario
-led += 3;
-
-
 
 void loop() {
-  digitalWrite(led, HIGH)
-  if(led >= 0)
-    Serial.write("Cadena");
-  else
-    Serial.wite("Otra cosa");
-    for(int i = 0; i < 10; i++){
-      algo += i << 2;
+  //digitalWrite(led, HIGH)
+  if(led >= 0){
+    write("Cadena");
     }
+  else{
+    write("Otra cosa");
+    for(i = 0; i < 10; i++){
+      algo = algo + 2;
+    }
+  }
 }
-*/
